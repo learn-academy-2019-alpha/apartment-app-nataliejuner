64f673a8f0360804f481a6aa6f82466b31df1315
Natalie Reinicke <nataliereinicke@Natalies-Air.localdomain>
2019-04-15 11:29:17 -0700

    adding devise

---
+++ app/javascript/components/AuthenticatedApp.js
@@ -0,0 +1,11 @@
+ import React from "react"
+ import PropTypes from "prop-types"
+ class AuthenticatedApp extends React.Component {
+   render () {
+     return (
+       <div>Authenticated</div>
+     );
+   }
+}
+
+export default AuthenticatedApp
